4d02d24475333f5f901be8df8a1bc4d9f1a5df30
Your Name <you@example.com>
2022-08-09 18:37:58 +0300

    Initial Commit

---
+++ FaceUpApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FaceUpApp
+//
+//  Created by Admin on 09.08.2022.
+//  Copyright © 2022 Admin. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
