8b6dd1c33b8b770298ecd1103123502438b69e33
Your Name <you@example.com>
2022-08-09 18:45:42 +0300

    init

--- FaceUpApp/ContentView.swift
+++ FaceUpApp/ContentView.swift
@@ -9,13 +9,37 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, World!")
+    
+    var body: some View{
+        
+        return HStack{
+            ForEach(0..<4) { index in
+                CardView(isFaceUp: true)
+            }
+        }
+        
+            .padding()
+            .foregroundColor(Color.orange)
+            .font(Font.largeTitle)
+            
+        
     }
+    
 }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+struct CardView:View {
+    var isFaceUp:Bool
+    
+    var body: some View{
+        ZStack{
+            if isFaceUp{
+                RoundedRectangle(cornerRadius: 10.0).fill(Color.white)
+                RoundedRectangle(cornerRadius:10.0).stroke(lineWidth: 3)
+                Text("👻")
+            }else{
+               RoundedRectangle(cornerRadius: 10.0).fill()
+            }
+        }
     }
+    
 }
