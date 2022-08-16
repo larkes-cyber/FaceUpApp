d8b94f1ae6af1973309716e2d7358732e90928e4
Your Name <playegrovoy@gmail.com>
2022-08-16 20:17:54 +0300

    some fiches

---
+++ FaceUpApp/Grid.swift
@@ -0,0 +1,37 @@
+//
+//  Grid.swift
+//  FaceUpApp
+//
+//  Created by Admin on 16.08.2022.
+//  Copyright © 2022 Admin. All rights reserved.
+//
+
+import SwiftUI
+
+struct Grid<Item, ItemView>: View where Item:Identifiable, ItemView:View {
+    
+    var items:[Item]
+    var viewForItem:(Item) -> ItemView
+    
+    init(_ items:[Item], viewForItem: @escaping (Item) -> ItemView){
+        self.items = items
+        self.viewForItem = viewForItem
+    }
+    
+    var body: some View {
+        GeometryReader{props in
+            self.body(for: GridLayout(itemCount: self.items.count, in: props.size))
+        }
+    }
+    
+    func body(for layout:GridLayout) -> some View{
+        ForEach(items){item in
+            self.body(for: item, in: layout)
+        }
+    }
+    
+    func body(for item:Item, in layout:GridLayout) -> some View{
+        return viewForItem(item)
+    }
+    
+}
